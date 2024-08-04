5d83fb73cc78fdbcc8c3301f469f4fb7d662bee2
pkp2207 <parampathak47@gmail.com>
2024-08-04 06:20:58 +0000

    Update projects styling

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,14 +17,14 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)} className='w-full sm:w-1/2 md:w-1/3 p-2'>
       <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-tertiary p-5 rounded-2xl w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
@@ -85,7 +85,7 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className='mt-20 flex flex-wrap gap-7'>
+      <div className='mt-20 flex flex-wrap'>
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
